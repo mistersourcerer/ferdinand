0ab23dd2657069778744773e3df0164613ef895c
Ricardo Valeriano <mister.sourcerer@gmail.com>
2023-09-01 00:51:00 -0300

    hello world

---
+++ lib/ferdinand/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Ferdinand
+  VERSION = "0.1.0"
+end
---
+++ spec/ferdinand_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Ferdinand do
+  it "has a version number" do
+    expect(Ferdinand::VERSION).not_to be nil
+  end
+end
