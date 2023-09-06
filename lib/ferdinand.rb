b6e83af6224aef55d94cffb87e579ca74133e424
Ricardo Valeriano <mister.sourcerer@gmail.com>
2023-09-06 00:55:35 -0300

    Brings zeitwerk to make loading stuff easy peasy.

--- lib/ferdinand.rb
+++ lib/ferdinand.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "ferdinand/version"
+require "zeitwerk"
+loader = Zeitwerk::Loader.for_gem
+loader.setup
 
 module Ferdinand
-  class Error < StandardError; end
-  # Your code goes here...
 end
