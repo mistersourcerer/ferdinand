68678cac1d4b09c6998df2ee885110eb436b1540
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-06-26 02:22:28 -0300

    Raises error if missing “}” on CHIP declaration.

--- spec/ferdinand/parser/syntax_spec.rb
+++ spec/ferdinand/parser/syntax_spec.rb
@@ -1,5 +1,8 @@
+require "ferdinand/parser/ast/pin"
+
 RSpec.describe Syntax do
   let(:tokenizer) { Scanner::Tokenizer.new(fixture("not.hdl")) }
+
   let(:ast) {
     Ast::Root.new(
       Ast::Chip.new("Not") {
@@ -23,6 +26,22 @@ RSpec.describe Syntax do
   describe "#analyse" do
     it "returns the adequate AST given a valid source hdl file" do
       expect(parser.analyse).to eq ast
+    end
+  end
+
+  context "errors" do
+    let(:tokenizer) { Scanner::Tokenizer.new(source) }
+    let(:parser) { described_class.new tokenizer }
+
+    context "missing right bracket" do
+      let(:source) { fixture("errors/missing_bracket.hdl") }
+      let(:error) do
+        "Expected a } [closing bracket] but none was found at[7:1]"
+      end
+
+      it "returns the adequate AST given a valid source hdl file" do
+        expect { parser.analyse }.to raise_error(SyntaxError, error)
+      end
     end
   end
 end
