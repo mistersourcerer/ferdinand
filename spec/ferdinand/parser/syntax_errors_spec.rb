d9bb97d4dca39a24a93a3cef19f92c1703c42802
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-06-28 13:57:05 -0300

    Ensure we are checking specific syntax errors.

--- spec/ferdinand/parser/syntax_errors_spec.rb
+++ spec/ferdinand/parser/syntax_errors_spec.rb
@@ -1,28 +1,39 @@
 require "ferdinand/parser/ast/pin"
 
+RSpec.shared_examples "parsing error" do
+  it "raises SyntaxError" do
+    expect { subject.analyse }.to raise_error(SyntaxError, error)
+  end
+end
+
 RSpec.describe Syntax, "Errors" do
   let(:tokenizer) { Scanner::Tokenizer.new(source) }
-  let(:parser) { described_class.new tokenizer }
+  subject { described_class.new tokenizer }
 
-  context "missing right bracket" do
+  describe "missing right bracket" do
     let(:source) { fixture("errors/missing_bracket.hdl") }
     let(:error) do
       "Expected a << } >> but none was found at [7:1]"
     end
 
-    it "raise error about missing closing bracket" do
-      expect { parser.analyse }.to raise_error(SyntaxError, error)
-    end
+    it_behaves_like "parsing error"
   end
 
-  context "missing left bracket" do
+  describe "missing left bracket" do
     let(:source) { fixture("errors/missing_open_bracket.hdl") }
     let(:error) do
       "Expected a << { >> but found << IN >> at [2:5]"
     end
 
-    it "raise error about missing closing bracket" do
-      expect { parser.analyse }.to raise_error(SyntaxError, error)
+    it_behaves_like "parsing error"
+  end
+
+  describe "missing chip name" do
+    let(:source) { fixture("errors/missing_chip_name.hdl") }
+    let(:error) do
+      "Expected a << NAME[str] >> but found << { >> at [1:6]"
     end
+
+    it_behaves_like "parsing error"
   end
 end
