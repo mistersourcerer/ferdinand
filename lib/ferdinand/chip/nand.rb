ada49ebe934cf4b989e37ed67fcb06260f4c5f2d
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-07-03 01:00:34 -0300

    :nail_care:

--- lib/ferdinand/chip/nand.rb
+++ lib/ferdinand/chip/nand.rb
@@ -1,25 +1,21 @@
-module Ferdinand
-  module Chip
-    class Nand
-      class Output
-        attr_reader :out
+class Ferdinand::Chip::Nand
+  class Output
+    attr_reader :out
 
-        def initialize(out:)
-          @out = out
-        end
-      end
-
-      def call(a:, b:)
-        out = (a == 1 && b == 1) ? 0 : 1
-
-        Output.new(out: out)
-      end
-
-      def pretty_print(q)
-        @_name ||= self.class.name.split("::").last
-        q.text "<chip> "
-        q.text @_name
-      end
+    def initialize(out:)
+      @out = out
     end
+  end
+
+  def call(a:, b:)
+    out = (a == 1 && b == 1) ? 0 : 1
+
+    Output.new(out: out)
+  end
+
+  def pretty_print(q)
+    @_name ||= self.class.name.split("::").last
+    q.text "<chip> "
+    q.text @_name
   end
 end
